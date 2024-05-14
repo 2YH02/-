4f283cdec69a4692e5bbb501d881b0f5d734bbbc
2YH02 <yonghuni484@gmail.com>
2024-05-14 17:45:40 +0900

    1, 2차원 탐색 항목 추가

---
+++ 2. 1, 2차원 탐색/4. 점수 계산/index.js
@@ -0,0 +1,7 @@
+function solution(arr) {
+  let answer;
+  return answer;
+}
+
+let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+console.log(solution(arr));
---
+++ 2. 1, 2차원 탐색/5. 등수 구하기/index.js
@@ -0,0 +1,7 @@
+function solution(arr) {
+  let answer;
+  return answer;
+}
+
+let arr = [87, 89, 92, 100, 76];
+console.log(solution(arr));
