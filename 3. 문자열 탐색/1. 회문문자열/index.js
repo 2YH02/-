ab39b460028d635112106e71b7544cbf85a87ab9
2YH02 <yonghuni484@gmail.com>
2024-05-14 19:37:15 +0900

    문자열 탐색 항목 추가

---
+++ 3. 문자열 탐색/1. 회문문자열/index.js
@@ -0,0 +1,7 @@
+function solution(s) {
+  let answer;
+  return answer;
+}
+
+let str = "goooG";
+console.log(solution(str));
---
+++ 3. 문자열 탐색/4. 가장 짧은 문자거리/index.js
@@ -0,0 +1,7 @@
+function solution(s, t) {
+  let answer;
+  return answer;
+}
+
+let str = "teachermode";
+console.log(solution(str, "e"));
