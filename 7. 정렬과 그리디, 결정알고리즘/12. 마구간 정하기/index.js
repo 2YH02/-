bccda8a5471416ad03971107c356b56ecb077f2c
2YH02 <yonghuni484@gmail.com>
2024-05-15 11:45:32 +0900

    정렬과 그리디, 결정알고리즘 항목 추가

---
+++ 7. 정렬과 그리디, 결정알고리즘/12. 마구간 정하기/index.js
@@ -0,0 +1,9 @@
+function count(stable, dist) {}
+
+function solution(c, stable) {
+  let answer;
+  return answer;
+}
+
+let arr = [1, 2, 8, 4, 9];
+console.log(solution(3, arr));
---
+++ 7. 정렬과 그리디, 결정알고리즘/4. 삽입정렬/index.js
@@ -0,0 +1,6 @@
+function solution1(arr) {
+  let answer;
+  return answer;
+}
+let arr = [11, 7, 5, 6, 10, 9];
+console.log(solution1(arr));
