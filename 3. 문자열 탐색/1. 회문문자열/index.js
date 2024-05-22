16c189a909dd7ef17f639da884e7cddefcf340d7
2YH02 <yonghuni484@gmail.com>
2024-05-22 23:30:01 +0900

    알고리즘 Day5

--- 3. 문자열 탐색/1. 회문문자열/index.js
+++ 3. 문자열 탐색/1. 회문문자열/index.js
@@ -1,5 +1,12 @@
 function solution(s) {
-  let answer;
+  let answer = "YES";
+  s = s.toLowerCase();
+  let len = s.length;
+
+  for (let i = 0; i < Math.floor(len / 2); i++) {
+    if (s[i] != s[len - i - 1]) return "NO";
+  }
+
   return answer;
 }
 
