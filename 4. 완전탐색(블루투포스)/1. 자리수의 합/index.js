9f7530185c1a158a1f17d5b57544762836314e5a
2YH02 <yonghuni484@gmail.com>
2024-05-27 18:43:09 +0900

    알고리즘 Day7

--- 4. 완전탐색(블루투포스)/1. 자리수의 합/index.js
+++ 4. 완전탐색(블루투포스)/1. 자리수의 합/index.js
@@ -1,7 +1,24 @@
 function solution(n, arr) {
   let answer;
+  let max = Number.MIN_SAFE_INTEGER;
+
+  for (let x of arr) {
+    let sum = 0;
+    let tmp = x;
+    while (tmp) {
+      sum += tmp % 10;
+      tmp = Math.floor(tmp / 10);
+    }
+
+    if (sum > max) {
+      max = sum;
+      answer = x;
+    } else if (sum === max) {
+      if (x > answer) answer = x;
+    }
+  }
   return answer;
 }
 
-let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
-console.log(solution(10, 3, arr));
+let arr = [128, 460, 603, 40, 521, 137, 123];
+console.log(solution(7, arr));
