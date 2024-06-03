a3ae6f6874cf164719b970b956858c0c62cb5799
2YH02 <yonghuni484@gmail.com>
2024-06-03 16:50:13 +0900

    알고리즘 Day9 4-5

--- 4. 완전탐색(블루투포스)/3. 멘토링/index.js
+++ 4. 완전탐색(블루투포스)/3. 멘토링/index.js
@@ -1,5 +1,24 @@
 function solution(test) {
-  let answer;
+  let answer = 0;
+  let m = test.length;
+  let n = test[0].length;
+
+  let path = [];
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      let cnt = 0;
+      for (let k = 0; k < m; k++) {
+        if (arr[k].indexOf(i) < arr[k].indexOf(j)) cnt++;
+      }
+
+      if (cnt === 3) {
+        answer++;
+        path.push([i, j]);
+      }
+    }
+  }
+  console.log(path);
   return answer;
 }
 
