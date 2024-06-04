5209d4ab375b1230abf5072916a7ad3963553d17
2YH02 <yonghuni484@gmail.com>
2024-06-04 18:03:37 +0900

    알고리즘 Day10 5-4

--- 5. 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/2. 공통원소 구하기/index.js
+++ 5. 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/2. 공통원소 구하기/index.js
@@ -1,5 +1,19 @@
 function solution(arr1, arr2) {
-  let answer;
+  let answer = [];
+  let n = arr1.length;
+  let p1 = (p2 = 0);
+
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+
+  while (p1 < n && p2 < n) {
+    if (arr1[p1] === arr2[p2]) {
+      answer.push(arr1[p1++]);
+      p2++;
+    } else if (arr1[p1] < arr2[p2]) p1++;
+    else p2++;
+  }
+
   return answer;
 }
 
