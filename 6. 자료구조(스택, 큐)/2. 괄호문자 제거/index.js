22c4f42beeb779467d3e33b7d53f93563ebe3500
2YH02 <yonghuni484@gmail.com>
2024-06-07 17:10:04 +0900

    알고리즘 Day13 6-3

--- 6. 자료구조(스택, 큐)/2. 괄호문자 제거/index.js
+++ 6. 자료구조(스택, 큐)/2. 괄호문자 제거/index.js
@@ -1,5 +1,17 @@
 function solution(s) {
-  let answer;
+  let answer = "";
+
+  let stack = [];
+
+  for (let x of s) {
+    if (x === ")") {
+      while (stack.pop() !== "(");
+    } else {
+      stack.push(x);
+    }
+  }
+
+  answer = stack.join("");
   return answer;
 }
 
