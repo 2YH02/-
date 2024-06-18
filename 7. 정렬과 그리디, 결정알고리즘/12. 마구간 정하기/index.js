32167e7ef62fd97f56b145793f06cc0f789a3e65
2YH02 <yonghuni484@gmail.com>
2024-06-18 15:50:35 +0900

    알고리즘 Day18 7-12

--- 7. 정렬과 그리디, 결정알고리즘/12. 마구간 정하기/index.js
+++ 7. 정렬과 그리디, 결정알고리즘/12. 마구간 정하기/index.js
@@ -1,7 +1,36 @@
-function count(stable, dist) {}
+function count(stable, dist) {
+  let cnt = 1;
+  let ep = stable[0];
+  for (let i = 0; i < stable.length; i++) {
+    if (stable[i] - ep >= dist) {
+      cnt++;
+      ep = stable[i];
+    }
+  }
+
+  return cnt;
+}
 
 function solution(c, stable) {
   let answer;
+  let n = stable.length;
+
+  stable.sort((a, b) => a - b);
+
+  let lt = 1;
+  let rt = stable[n - 1];
+
+  while (lt <= rt) {
+    let mid = Math.floor((lt + rt) / 2);
+
+    if (count(stable, mid) >= c) {
+      answer = mid;
+      lt = mid + 1;
+    } else {
+      rt = mid - 1;
+    }
+  }
+
   return answer;
 }
 
--- 7. 정렬과 그리디, 결정알고리즘/9. 결혼식/index.js
+++ 7. 정렬과 그리디, 결정알고리즘/9. 결혼식/index.js
@@ -1,5 +1,24 @@
 function solution(times) {
-  let answer;
+  let answer = 0;
+  let T_line = [];
+
+  for (let x of times) {
+    T_line.push([x[0], "s"]);
+    T_line.push([x[1], "e"]);
+  }
+
+  T_line.sort((a, b) => {
+    return a[0] === b[0] ? a[1].charCodeAt() - b[1].charCodeAt() : a[0] - b[0];
+  });
+
+  let tmp = 0;
+  for (let x of T_line) {
+    if (x[1] === "s") tmp++;
+    else tmp--;
+
+    answer = Math.max(tmp, answer);
+  }
+
   return answer;
 }
 
