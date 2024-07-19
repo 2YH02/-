e2bb81dca6dc916eb81f224c5f9c69004b22a4e7
2YH02 <yonghuni484@gmail.com>
2024-07-19 15:08:13 +0900

    6-7

--- 6. 자료구조(스택, 큐)/5. 쇠막대기/solution.js
+++ 6. 자료구조(스택, 큐)/5. 쇠막대기/solution.js
@@ -6,7 +6,7 @@ function solution(s) {
     else {
       stack.pop();
       if (s[i - 1] === "(") answer += stack.length;
-      else answer++;
+      else answer++; // 막대기 끝나는 지점인지 확인
       //stack.pop(); 이 위치에 하면 레이저까지 카운팅한다.
     }
   }
