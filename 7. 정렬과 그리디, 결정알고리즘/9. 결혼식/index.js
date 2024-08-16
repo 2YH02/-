783e520115f73f2b40d72a6fd53d3c2702affdde
2YH02 <yonghuni484@gmail.com>
2024-08-16 22:07:08 +0900

    7-9

--- 7. 정렬과 그리디, 결정알고리즘/9. 결혼식/index.js
+++ 7. 정렬과 그리디, 결정알고리즘/9. 결혼식/index.js
@@ -1,5 +1,24 @@
 function solution(times) {
-  let answer = 0;
+  let answer = Number.MIN_SAFE_INTEGER;
+  let T_line = [];
+
+  times.forEach((v) => {
+    T_line.push([v[0], "s"]);
+    T_line.push([v[1], "e"]);
+  });
+
+  T_line.sort((a, b) =>
+    a[0] === b[0] ? a[1].charCodeAt() - b[1].charCodeAt() : a[0] - b[0]
+  );
+
+  let cnt = 0;
+  T_line.forEach((v, i) => {
+    if (v[1] === "s") cnt++;
+    else cnt--;
+
+    answer = Math.max(answer, cnt);
+  });
+
   return answer;
 }
 
