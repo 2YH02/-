18620d694673797abb20c951ec593345dc3a0833
2YH02 <yonghuni484@gmail.com>
2024-08-22 19:03:27 +0900

    8-3

--- 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/3. 이진트리 순회/index.js
+++ 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/3. 이진트리 순회/index.js
@@ -1,5 +1,16 @@
 function solution(n) {
   let answer = "";
+
+  const DFS = (v) => {
+    if (v > 7) return;
+    else {
+      DFS(v * 2);
+      DFS(v * 2 + 1);
+      console.log(v);
+    }
+  };
+
+  DFS(n);
   return answer;
 }
 
