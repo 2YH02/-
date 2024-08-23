0b1f476500a3da39f97d2a42042e05443ac4e0f2
2YH02 <yonghuni484@gmail.com>
2024-08-23 20:04:14 +0900

    8-4

--- 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/4. 부분집합 구하기/index.js
+++ 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/4. 부분집합 구하기/index.js
@@ -1,5 +1,23 @@
 function solution(n) {
   let answer = [];
+  let ch = Array.from({ length: n + 1 }, () => 0);
+
+  const DFS = (L) => {
+    if (L === n + 1) {
+      let tmp = [];
+      for (let i = 1; i <= n; i++) {
+        if (ch[i] === 1) tmp.push(i);
+      }
+      if (tmp.length > 0) answer.push(tmp);
+    } else {
+      ch[L] = 1;
+      DFS(L + 1);
+      ch[L] = 0;
+      DFS(L + 1);
+    }
+  };
+
+  DFS(1);
   return answer;
 }
 
