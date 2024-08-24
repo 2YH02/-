91224a1a0229a33266f326aff8a6df649b5fadb6
2YH02 <yonghuni484@gmail.com>
2024-08-24 12:22:31 +0900

    8-8

--- 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/5. 합이 같은 부분집합/index.js
+++ 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/5. 합이 같은 부분집합/index.js
@@ -1,5 +1,23 @@
 function solution(arr) {
-  let answer;
+  let answer = "NO";
+  let n = arr.length;
+  let total = arr.reduce((a, b) => a + b);
+  let flag = 0;
+
+  const DFS = (L, sum) => {
+    if (flag) return;
+    if (L === n) {
+      if (sum === parseInt(total / 2)) {
+        answer = "YES";
+        flag = 1;
+      }
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  };
+
+  DFS(0, 0);
   return answer;
 }
 
--- 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/8. 중복순열/index.js
+++ 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/8. 중복순열/index.js
@@ -1,5 +1,19 @@
 function solution(n, m) {
   let answer = [];
+  let ch = Array.from({ length: m }, () => 0);
+
+  const DFS = (L) => {
+    if (L === m) {
+      answer.push(ch.slice());
+    } else {
+      for (let i = 1; i <= n; i++) {
+        ch[L] = i;
+        DFS(L + 1);
+      }
+    }
+  };
+
+  DFS(0);
   return answer;
 }
 
