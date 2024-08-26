b724ebe4d7db2572af08cd8ab8f6d8b2612777ea
2YH02 <yonghuni484@gmail.com>
2024-08-26 18:46:15 +0900

    8-9

--- 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/9. 동전교환/index.js
+++ 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/9. 동전교환/index.js
@@ -1,5 +1,21 @@
 function solution(m, arr) {
-  let answer;
+  let answer = Number.MAX_SAFE_INTEGER;
+  let n = arr.length;
+
+  const DFS = (L, sum) => {
+    if (sum > m) return;
+    if (L >= answer) return;
+
+    if (sum === m) {
+      answer = Math.min(answer, L);
+    } else {
+      for (let i = 0; i < n; i++) {
+        DFS(L + 1, sum + arr[i]);
+      }
+    }
+  };
+
+  DFS(0, 0);
   return answer;
 }
 
