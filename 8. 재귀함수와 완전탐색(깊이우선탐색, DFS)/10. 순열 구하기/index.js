a1eff96929511a00b5dc75dc816f7f1a2fc3529f
2YH02 <yonghuni484@gmail.com>
2024-08-27 15:38:49 +0900

    8-12

--- 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/10. 순열 구하기/index.js
+++ 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/10. 순열 구하기/index.js
@@ -1,5 +1,26 @@
 function solution1(m, arr) {
   let answer = [];
+  let n = arr.length;
+  let ch = Array.from({ length: n }, () => 0);
+
+  let tmp = Array.from({ length: m }, () => 0);
+
+  const DFS = (L) => {
+    if (L === m) {
+      answer.push(tmp.slice());
+    } else {
+      for (let i = 0; i < n; i++) {
+        if (ch[i] === 0) {
+          ch[i] = 1;
+          tmp[L] = arr[i];
+          DFS(L + 1);
+          ch[i] = 0;
+        }
+      }
+    }
+  };
+
+  DFS(0);
   return answer;
 }
 
--- 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/11. 팩토리얼/index.js
+++ 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/11. 팩토리얼/index.js
@@ -1,5 +1,12 @@
 function solution(n) {
   let answer;
+
+  const DFS = (L) => {
+    if (L === 1) return 1;
+    else return L * DFS(L - 1);
+  };
+
+  answer = DFS(n);
   return answer;
 }
 
