499b4cb2a54ed436446e0f3102942e5415c3ec5c
2YH02 <yonghuni484@gmail.com>
2024-09-02 16:21:26 +0900

    9-4

--- 9. 그래프와 탐색(DFS, BFS)/4. 이진트리 넓이우선탐색(BFS)/index.js
+++ 9. 그래프와 탐색(DFS, BFS)/4. 이진트리 넓이우선탐색(BFS)/index.js
@@ -1,5 +1,20 @@
 function solution() {
   let answer = "";
+  let queue = [];
+  queue.push(1);
+
+  while (queue.length) {
+    console.log(queue);
+
+    let v = queue.shift();
+    answer += v + " ";
+
+    for (let nv of [v * 2, v * 2 + 1]) {
+      if (nv > 7) continue;
+      queue.push(nv);
+    }
+  }
+
   return answer;
 }
 
