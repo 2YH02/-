c0662c056c15812cbcd3461af0d4ec7dc3f99aa0
2YH02 <yonghuni484@gmail.com>
2024-10-13 16:38:52 +0900

    6-5

--- 6. 자료구조(스택, 큐)/4. 후위식 연산/index.js
+++ 6. 자료구조(스택, 큐)/4. 후위식 연산/index.js
@@ -1,5 +1,25 @@
 function solution(s) {
   let answer;
+  let stack = [];
+
+  for (let x of s) {
+    if (!isNaN(x)) stack.push(Number(x));
+    else {
+      let r = stack.pop();
+      let l = stack.pop();
+      if (x === "+") {
+        stack.push(l + r);
+      } else if (x === "-") {
+        stack.push(l - r);
+      } else if (x === "*") {
+        stack.push(l * r);
+      } else if (x === "/") {
+        stack.push(l / r);
+      }
+    }
+  }
+
+  answer = stack[0];
   return answer;
 }
 
