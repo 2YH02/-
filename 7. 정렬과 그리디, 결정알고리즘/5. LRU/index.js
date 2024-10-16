f7c7448c7f2f8ee359e92c3cdd2f5c49f254b67d
2YH02 <yonghuni484@gmail.com>
2024-10-16 20:29:50 +0900

    7-5

--- 7. 정렬과 그리디, 결정알고리즘/5. LRU/index.js
+++ 7. 정렬과 그리디, 결정알고리즘/5. LRU/index.js
@@ -1,5 +1,17 @@
 function solution(size, arr) {
-  let answer;
+  let answer = [];
+
+  for (let x of arr) {
+    let index = answer.indexOf(x);
+    if (index === -1) {
+      answer.unshift(x);
+      if (answer.length > size) answer.pop();
+    } else {
+      answer.splice(index, 1);
+      answer.unshift(x);
+    }
+  }
+
   return answer;
 }
 
