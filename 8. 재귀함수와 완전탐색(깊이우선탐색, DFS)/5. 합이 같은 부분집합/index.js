3af282d937d7c2ea95e0d836a428b1fc2d12fa54
2YH02 <yonghuni484@gmail.com>
2024-10-29 21:36:03 +0900

    8-6

--- 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/5. 합이 같은 부분집합/index.js
+++ 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/5. 합이 같은 부분집합/index.js
@@ -1,5 +1,20 @@
 function solution(arr) {
-  let answer;
+  let answer = "NO!";
+  let total = arr.reduce((a, b) => a + b);
+  let n = arr.length;
+
+  const DFS = (L, sum) => {
+    if (answer === "YES!" || L > n || sum > total / 2) return;
+    if (total / 2 === sum) {
+      answer = "YES!";
+      return;
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  };
+
+  DFS(0, 0);
   return answer;
 }
 
--- 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/5. 합이 같은 부분집합/solution.js
+++ 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/5. 합이 같은 부분집합/solution.js
@@ -21,3 +21,23 @@ function solution(arr) {
 
 let arr = [1, 3, 5, 6, 7, 10];
 console.log(solution(arr));
+
+// function solution(arr) {
+//   let answer = "NO";
+//   let total = arr.reduce((a, b) => a + b);
+//   let n = arr.length;
+
+//   function DFS(L, sum) {
+//     if (answer == "YES" || L > n ||sum > total / 2) return;
+
+//     if (total / 2 === sum) {
+//       answer = "YES";
+//       return;
+//     } else {
+//       DFS(L + 1, sum + arr[L]);
+//       DFS(L + 1, sum);
+//     }
+//   }
+//   DFS(0, 0);
+//   return answer;
+// }
