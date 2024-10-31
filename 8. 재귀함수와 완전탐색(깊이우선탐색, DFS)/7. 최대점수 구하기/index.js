6cbe7d590653fa4b92f0062cf7f01d84c3c0c841
2YH02 <yonghuni484@gmail.com>
2024-10-31 17:16:32 +0900

    8-7

--- 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/7. 최대점수 구하기/index.js
+++ 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/7. 최대점수 구하기/index.js
@@ -1,5 +1,18 @@
 function solution(m, ps, pt) {
-  let answer;
+  let answer = 0;
+  let n = ps.length;
+
+  const DFS = (L, s, t) => {
+    if (t > m) return;
+    if (L === n) {
+      answer = Math.max(answer, s);
+    } else {
+      DFS(L + 1, s + ps[L], t + pt[L]);
+      DFS(L + 1, s, t);
+    }
+  };
+
+  DFS(0, 0, 0);
   return answer;
 }
 
