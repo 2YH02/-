a640abff78f4ac6727e44753f0afcd0c8bbda965
2YH02 <yonghuni484@gmail.com>
2024-11-08 19:36:42 +0900

    9-5

--- 9. 그래프와 탐색(DFS, BFS)/5. 송아지 찾기(BFS)/index.js
+++ 9. 그래프와 탐색(DFS, BFS)/5. 송아지 찾기(BFS)/index.js
@@ -1,6 +1,28 @@
 function solution(s, e) {
-  let answer;
+  let answer = -1;
+  let visited = new Set();
+
+  let queue = [];
+
+  queue.push([s, 0]);
+
+  while (queue.length) {
+    let [v, dist] = queue.shift();
+
+    if (v === e) {
+      answer = dist;
+      break;
+    }
+
+    for (let nv of [v + 1, v - 1, v + 5]) {
+      if (!visited.has(nv) && nv >= 0) {
+        visited.add(nv);
+        queue.push([nv, dist + 1]);
+      }
+    }
+  }
+
   return answer;
 }
 
-console.log(solution(5, 14));
+console.log(solution(8, 3));
