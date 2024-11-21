f2144e67bf53ddfa9d3650a2a7af1e575cf475af
2YH02 <yonghuni484@gmail.com>
2024-11-21 19:20:37 +0900

    1-9

--- 1. 기본문제/8. 일곱난쟁이/index.js
+++ 1. 기본문제/8. 일곱난쟁이/index.js
@@ -1,5 +1,23 @@
 function solution(arr) {
   let answer = arr;
+  let n = arr.length;
+  let total = arr.reduce((a, b) => a + b);
+
+  let flag = false;
+
+  for (let i = 0; i < n - 1; i++) {
+    if (flag) break;
+    for (let j = i + 1; j < n; j++) {
+      let sum = arr[i] + arr[j];
+
+      if (total - sum === 100) {
+        arr.splice(j, 1);
+        arr.splice(i, 1);
+        flag = 1;
+        break;
+      }
+    }
+  }
   return answer;
 }
 
