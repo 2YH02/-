ebe747d1753e00854a7f5fd54ab93fd2874c18af
2YH02 <yonghuni484@gmail.com>
2024-11-24 22:45:56 +0900

    1-15

--- 1. 기본문제/14. 가장 긴 문자열/index.js
+++ 1. 기본문제/14. 가장 긴 문자열/index.js
@@ -1,5 +1,13 @@
 function solution(s) {
   let answer;
+  let maxLength = 0;
+
+  for (let x of s) {
+    if (x.length > maxLength) {
+      answer = x;
+      maxLength = x.length;
+    }
+  }
   return answer;
 }
 let str = ["teacher", "time", "student", "beautiful", "good"];
--- 1. 기본문제/15. 가운데 문자 출력/index.js
+++ 1. 기본문제/15. 가운데 문자 출력/index.js
@@ -1,5 +1,12 @@
 function solution(s) {
   let answer = "";
+  let mid = Math.ceil(s.length / 2);
+
+  if (mid % 2 === 1) {
+    answer = s.slice(mid - 1, mid);
+  } else {
+    answer = s.slice(mid - 1, mid + 1);
+  }
   return answer;
 }
-console.log(solution("study"));
+console.log(solution("stdy"));
