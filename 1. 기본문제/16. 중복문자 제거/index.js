61b73234c2b4aed01de33b363377f775f8ae221f
2YH02 <yonghuni484@gmail.com>
2024-11-25 17:32:36 +0900

    1-17

--- 1. 기본문제/16. 중복문자 제거/index.js
+++ 1. 기본문제/16. 중복문자 제거/index.js
@@ -1,5 +1,8 @@
 function solution(s) {
   let answer = "";
+  const a = new Set(s);
+
+  answer = Array.from(a).join("");
   return answer;
 }
 console.log(solution("ksekkset"));
--- 1. 기본문제/17. 중복단어 제거/index.js
+++ 1. 기본문제/17. 중복단어 제거/index.js
@@ -1,5 +1,10 @@
 function solution(s) {
   let answer;
+
+  answer = s.filter((v, i) => {
+    return s.indexOf(v) === i;
+  });
+  
   return answer;
 }
 let str = ["good", "time", "good", "time", "student"];
