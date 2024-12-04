8ba6a4efd994907e094ac0c0a4197825c70ce608
2YH02 <yonghuni484@gmail.com>
2024-12-04 17:57:13 +0900

    4-2

--- 4. 완전탐색(블루투포스)/1. 자리수의 합/index.js
+++ 4. 완전탐색(블루투포스)/1. 자리수의 합/index.js
@@ -1,5 +1,24 @@
 function solution(n, arr) {
-  let answer = "";
+  let answer = 0;
+  let max = Number.MIN_SAFE_INTEGER;
+
+  for (let x of arr) {
+    let tmp = x;
+    let sum = 0;
+
+    while (tmp) {
+      sum += tmp % 10;
+      tmp = ~~(tmp / 10);
+    }
+
+    if (sum > max) {
+      max = sum;
+      answer = x;
+    } else if (sum === max) {
+      if (x > answer) answer = x;
+    }
+  }
+
   return answer;
 }
 
