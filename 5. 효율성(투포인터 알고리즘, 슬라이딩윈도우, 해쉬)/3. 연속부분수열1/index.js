1f398c5099e0b05a0bb2cb71307f1548dd4a64a0
2YH02 <yonghuni484@gmail.com>
2024-12-16 11:33:55 +0900

    5-3

--- 5. 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/3. 연속부분수열1/index.js
+++ 5. 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/3. 연속부분수열1/index.js
@@ -1,5 +1,16 @@
 function solution(m, arr) {
   let answer = 0;
+  let lt = 0;
+  let sum = 0;
+
+  for (let rt = 0; rt < arr.length; rt++) {
+    sum += arr[rt];
+    if (sum === m) answer++;
+    while (sum >= m) {
+      sum -= arr[lt++];
+      if (sum === m) answer++;
+    }
+  }
   return answer;
 }
 
