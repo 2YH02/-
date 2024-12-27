11707cdafe3a720ef64c45e837393d626e44a4a5
2YH02 <yonghuni484@gmail.com>
2024-12-27 15:13:37 +0900

    5-6

--- 5. 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/5. 최대 매출/index.js
+++ 5. 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/5. 최대 매출/index.js
@@ -1,5 +1,17 @@
 function solution(k, arr) {
   let answer = 0;
+  let sum = 0;
+
+  for (let i = 0; i < k; i++) {
+    sum += arr[i];
+  }
+
+  answer = sum;
+
+  for (let i = k; i < arr.length; i++) {
+    sum += arr[i] - arr[i - k];
+    answer = Math.max(answer, sum);
+  }
   return answer;
 }
 
--- 5. 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/6. 학급 회장(해쉬)/index.js
+++ 5. 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/6. 학급 회장(해쉬)/index.js
@@ -1,5 +1,19 @@
 function solution(s) {
   let answer = "";
+  let sH = new Map();
+
+  for (let x of s) {
+    if (sH.has(x)) sH.set(x, sH.get(x) + 1);
+    else sH.set(x, 1);
+  }
+
+  let max = Number.MIN_SAFE_INTEGER;
+  for (let [k, v] of sH) {
+    if (v > max) {
+      max = v;
+      answer = k;
+    }
+  }
   return answer;
 }
 
