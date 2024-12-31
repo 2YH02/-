1bca34c64d85a0b92bcd13f18ffc0ce9474587d0
2YH02 <yonghuni484@gmail.com>
2024-12-31 14:11:13 +0900

    6-4

--- 6. 자료구조(스택, 큐)/1. 올바른 괄호/index.js
+++ 6. 자료구조(스택, 큐)/1. 올바른 괄호/index.js
@@ -1,5 +1,18 @@
 function solution(s) {
   let answer = "YES";
+
+  let stack = [];
+
+  for (let x of s) {
+    if (x === "(") stack.push(x);
+    else {
+      if (stack.length === 0) return "NO";
+      stack.pop();
+    }
+  }
+
+  if (stack.length > 0) answer = "NO";
+
   return answer;
 }
 
--- 6. 자료구조(스택, 큐)/3. 크레인 인형뽑기(카카오)/index.js
+++ 6. 자료구조(스택, 큐)/3. 크레인 인형뽑기(카카오)/index.js
@@ -1,5 +1,21 @@
 function solution(board, moves) {
   let answer = 0;
+
+  let stack = [];
+
+  for (let x of moves) {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][x - 1] !== 0) {
+        let tmp = board[i][x - 1];
+        board[i][x - 1] = 0;
+        if (tmp === stack[stack.length - 1]) {
+          stack.pop();
+          answer += 2;
+        } else stack.push(tmp);
+        break;
+      }
+    }
+  }
   return answer;
 }
 
