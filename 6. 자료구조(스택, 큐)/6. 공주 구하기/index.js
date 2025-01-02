8cd9118b247649d31ede4a7081fc4ee0537fc85a
2YH02 <yonghuni484@gmail.com>
2025-01-02 17:17:15 +0900

    6-7

--- 6. 자료구조(스택, 큐)/6. 공주 구하기/index.js
+++ 6. 자료구조(스택, 큐)/6. 공주 구하기/index.js
@@ -1,5 +1,14 @@
 function solution(n, k) {
   let answer = 0;
+  let queue = Array.from({ length: n }, (_, i) => i + 1);
+
+  while (queue.length) {
+    for (let i = 0; i < k; i++) {
+      queue.push(queue.shift());
+    }
+    queue.pop();
+    if (queue.length === 1) answer = queue.shift();
+  }
   return answer;
 }
 
--- 6. 자료구조(스택, 큐)/7. 교육과정 설계/index.js
+++ 6. 자료구조(스택, 큐)/7. 교육과정 설계/index.js
@@ -1,5 +1,14 @@
 function solution(need, plan) {
   let answer = "YES";
+  let queue = need.split("");
+
+  for (let x of plan) {
+    if (queue.includes(x)) {
+      if (queue.shift() !== x) return "NO";
+    }
+  }
+
+  if (queue.length > 0) return "NO";
   return answer;
 }
 
