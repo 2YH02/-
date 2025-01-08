c9bebc2b5ebd42597433797371542828fc300c22
2YH02 <yonghuni484@gmail.com>
2025-01-08 23:28:21 +0900

    7-7

--- 7. 정렬과 그리디, 결정알고리즘/5. 병합정렬/index.js
+++ 7. 정렬과 그리디, 결정알고리즘/5. 병합정렬/index.js
@@ -1,4 +1,27 @@
-function solution(arr) {}
+function merge(left, right) {
+  const sortedArr = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) sortedArr.push(left[i++]);
+    else sortedArr.push(right[j++]);
+  }
+
+  return sortedArr.concat(left.slice(i)).concat(right.slice(j));
+}
+
+function solution(arr) {
+  if (arr.length === 1) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid);
+
+  return merge(solution(left), solution(right));
+}
 
 let arr = [13, 5, 11, 7, 23, 15, 2];
 console.log(solution(arr));
--- 7. 정렬과 그리디, 결정알고리즘/6. 장난꾸러기 현수/index.js
+++ 7. 정렬과 그리디, 결정알고리즘/6. 장난꾸러기 현수/index.js
@@ -1,5 +1,13 @@
 function solution(arr) {
   let answer = [];
+
+  let sortArr = arr.slice();
+  sortArr.sort((a, b) => a - b);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== sortArr[i]) answer.push(i + 1);
+  }
+
   return answer;
 }
 
