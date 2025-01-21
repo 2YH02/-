dcc6fbc3ab5f51b4660baefa656ce80b494944aa
2YH02 <yonghuni484@gmail.com>
2025-01-21 23:48:02 +0900

    8-2

--- 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/2. 이진수 출력(재귀)/index.js
+++ 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/2. 이진수 출력(재귀)/index.js
@@ -1,5 +1,16 @@
 function solution(n) {
   let answer = "";
+
+  const DFS = (n) => {
+    if (n === 0) return;
+    else {
+      DFS(parseInt(n / 2));
+      answer += String(n % 2);
+    }
+  };
+
+  DFS(n);
+
   return answer;
 }
 
