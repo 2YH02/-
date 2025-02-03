2dadd5078f0246b9d39ad4e8deee8b5ecab1f55f
2YH02 <yonghuni484@gmail.com>
2025-02-03 21:18:04 +0900

    8-5

--- 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/5. 합이 같은 부분집합/index.js
+++ 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/5. 합이 같은 부분집합/index.js
@@ -1,5 +1,25 @@
 function solution(arr) {
-  let answer = "NO!";
+  let answer = "NO";
+  let flag = 0;
+  let total = arr.reduce((a, b) => a + b, 0);
+
+  let n = arr.length;
+
+  const DFS = (L, sum) => {
+    if (flag) return;
+    if (L === n) {
+      if (total - sum === sum) {
+        answer = "YES";
+        flag = 1;
+      }
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  };
+
+  DFS(0, 0);
+
   return answer;
 }
 
